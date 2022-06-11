01a9333b301d01f1aafee0ae89db7047c7774069
alejandro barroso bueso <you@example.com>
2022-06-11 23:55:23 +0200

    11/06/2022

--- js/diseñosFotos.js
+++ js/diseñosFotos.js
@@ -1,7 +1,9 @@
 const banos = document.getElementById("banos");
 const cocinas = document.getElementById("cocinas");
+const showroom = document.getElementById("showroom");
 let b = 0;
 let c = 0;
+let l = 0;
 function fotos(tipo,nombreid,carpeta,n,s,t){
     const contenedorGaleria = nombreid;
     for(i = s; i<n;i++){
@@ -28,12 +30,19 @@ function fotos(tipo,nombreid,carpeta,n,s,t){
     if(tipo == "bano"){
         b = n;
     }
+    if(tipo == "showroom"){
+        l = n;
+    }
 }
 function cocina(){
-    fotos("cocina",cocinas,"imagenesOptimizadas/Cocinas/ordenadas",c+6,c,40);
+    fotos("cocina",cocinas,"imagenesOptimizadas/Cocinas/ordenadas",c+6,c,35,);
 }
 function bano(){
     fotos("bano",banos,"imagenesOptimizadas/Baños/ordenadas",b+6,b,16);
 }
+function showroo(){
+    fotos("showroom",showroom,"showroom/aShowroomOrdenada",l+6,l,16);
+}
 fotos("cocina",banos,"imagenesOptimizadas/Baños/ordenadas",7,1,39);
 fotos("bano",cocinas,"imagenesOptimizadas/Cocinas/ordenadas",7,1,16);
+fotos("showroom",showroom,"showroom/aShowroomOrdenadas",7,1,16);
