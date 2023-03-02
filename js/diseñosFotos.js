21b2969f8fd1e714f05134d56b79a917e8176597
alejandro barroso bueso <you@example.com>
2023-03-02 19:50:51 +0100

    Change JPG to jpg.

--- js/diseñosFotos.js
+++ js/diseñosFotos.js
@@ -4,45 +4,45 @@ const showroom = document.getElementById("showroom");
 let b = 0;
 let c = 0;
 let l = 0;
-function fotos(tipo,nombreid,carpeta,n,s,t){
+function fotos(tipo, nombreid, carpeta, n, s, t) {
     const contenedorGaleria = nombreid;
-    for(i = s; i<n;i++){
-        if(i<t)
-        {const article = document.createElement("article");
-        article.className = "fotos"
-        const link = document.createElement("a");
-        link.href="imagenes/"+carpeta+"/foto"+ i +".JPG";
-        article.className = "fotos";
-        const img = document.createElement("img");
-        img.src = "imagenes/"+carpeta+"/foto"+ i +".JPG";
-        img.alt = "imagenOptimizada";
-        img.loading = "lazy";
-        link.appendChild(img);
-        article.appendChild(link);
-        contenedorGaleria.appendChild(article);
-    }else{
-        document.getElementById("boton"+tipo).style.display = "none";
+    for (i = s; i < n; i++) {
+        if (i < t) {
+            const article = document.createElement("article");
+            article.className = "fotos"
+            const link = document.createElement("a");
+            link.href = "imagenes/" + carpeta + "/foto" + i + ".jpg";
+            article.className = "fotos";
+            const img = document.createElement("img");
+            img.src = "imagenes/" + carpeta + "/foto" + i + ".jpg";
+            img.alt = "imagenOptimizada";
+            img.loading = "lazy";
+            link.appendChild(img);
+            article.appendChild(link);
+            contenedorGaleria.appendChild(article);
+        } else {
+            document.getElementById("boton" + tipo).style.display = "none";
+        }
     }
-    }
-    if (tipo == "cocina"){
+    if (tipo == "cocina") {
         c = n;
     }
-    if(tipo == "bano"){
+    if (tipo == "bano") {
         b = n;
     }
-    if(tipo == "showroom"){
+    if (tipo == "showroom") {
         l = n;
     }
 }
-function cocina(){
-    fotos("cocina",cocinas,"imagenesOptimizadas/Cocinas/ordenadas",c+6,c,35,);
+function cocina() {
+    fotos("cocina", cocinas, "imagenesOptimizadas/Cocinas/ordenadas", c + 6, c, 35,);
 }
-function bano(){
-    fotos("bano",banos,"imagenesOptimizadas/Baños/ordenadas",b+6,b,16);
+function bano() {
+    fotos("bano", banos, "imagenesOptimizadas/Baños/ordenadas", b + 6, b, 16);
 }
-function showroo(){
-    fotos("showroom",showroom,"showroom/aShowroomOrdenada",l+6,l,16);
+function showroo() {
+    fotos("showroom", showroom, "showroom/aShowroomOrdenada", l + 6, l, 16);
 }
-fotos("cocina",banos,"imagenesOptimizadas/Baños/ordenadas",7,1,39);
-fotos("bano",cocinas,"imagenesOptimizadas/Cocinas/ordenadas",7,1,16);
-fotos("showroom",showroom,"showroom/aShowroomOrdenadas",7,1,16);
+fotos("cocina", banos, "imagenesOptimizadas/Baños/ordenadas", 7, 1, 39);
+fotos("bano", cocinas, "imagenesOptimizadas/Cocinas/ordenadas", 7, 1, 16);
+fotos("showroom", showroom, "showroom/aShowroomOrdenadas", 7, 1, 16);
--- js/sumarfotos.js
+++ js/sumarfotos.js
@@ -4,15 +4,11 @@ function fotos(n){
         const article = document.createElement("article");
         article.className = "fotos";
         const img = document.createElement("img");
-        img.src = "imagenes/imagenesOptimizadas/foto"+ i +".JPG";
+        img.src = "imagenes/imagenesOptimizadas/foto"+ i +".jpg";
         img.alt = "imagenOptimizada";
         img.loading = "lazy";
         article.appendChild(img);
-        if(i==45||i==46){
-            console.log("hola");
-        }else{
-            contenedorGaleria.appendChild(article);
-        }
+        contenedorGaleria.appendChild(article);
     }
 }
 fotos(52);
